8ead10fe5774197a35b4f80180fce973bb58666a
artiom1991 <a.stoilos@mail.ru>
2024-10-02 22:18:09 +0300

    first commit

--- quicksort.py
+++ quicksort.py
@@ -3,9 +3,9 @@ import random
 
 
 def quicksort_old(array):
-    if len(array) < 2:
+    if len(array) < 2:  # базовый случай
         return array
-    else:
+    else:               # рекурсивный случай
         pivot = array[0]
         less = [i for i in array[1:] if i < pivot]
         greater = [i for i in array[1:] if i > pivot]
@@ -16,20 +16,26 @@ def quicksort_new(array):
     if len(array) < 2:
         return array
     else:
-        pivot = random.choice(array)
+        pivot = random.choice(array) # выбор случайного опорного элемента
         less = [i for i in array if i < pivot]
         greater = [i for i in array if i > pivot]
         return quicksort_new(less) + [pivot] + quicksort_new(greater)
 
 
+
+# Если передавать отсортированый список, или слабо отсортированый то это алгоритм работает очень медленно
+# В данном случае он отрабатывает за O(n*n) что фактически медленнее чем сортировка выбором
+# Проблема в том что опорным элементом выбирается первый элемент списка
 start = time.time()
-quicksort_old([i for i in range(1, 1_000)])
+arr_old = quicksort_old([i for i in range(1, 1_000)])
 end = time.time()
 print("Old quicksort time:", end - start)
 # RecursionError: maximum recursion depth exceeded
 
+# Во втором примере случайным образом выбирается опорный элемент, что позволяет алгоритму
+# даже в слабо отсортированном списке отработать за максимально короткое время, что приближает алгоритм к O(n * log n)
 start = time.time()
-quicksort_new([i for i in range(1, 1_000)])
+arr_new = quicksort_new([i for i in range(1, 1_000)])
 end = time.time()
 print("New quicksort time:", end - start)
 
--- selection_sort.py
+++ selection_sort.py
@@ -1,5 +1,7 @@
 import time
 
+
+
 def findSmallest(arr):
     smallest = arr[0]
     smallest_index = 0
@@ -11,14 +13,23 @@ def findSmallest(arr):
 
 def selectionSort(arr):
     newArr = []
+    i1 = 0
     for i in range(len(arr)):
+        i1 += len(arr)
         smallest = findSmallest(arr)
         newArr.append(arr.pop(smallest))
+    print("Всего элементов было отсортировано: ", i1)
     return newArr
 
 
 start = time.time()
-selectionSort([i for i in range(1, 1_000)])
+selectionSort([i for i in range(0, 1_000)])
 end = time.time()
-print(end - start)
+print("Время выполнения алгоритма: ", end - start)
+
+
+# Теоретическая скорость отработки этого алгоритма O(n*n) или O(n^2)
+# В нашем случае это 1_000 * 1_000 или 1_000^2 это равно 1_000_000 операций
+# Однако реальная скорость работы немного быстрее, а именно O( (n * (n+1)) / 2 )
+# что в результате для 1_000 элементов дает 500500 итераций
 
