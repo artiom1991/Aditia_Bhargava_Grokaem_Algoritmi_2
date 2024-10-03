f7290ce6bc13c6829d13edc969f7dd5dbd4955a2
artiom1991 <a.stoilos@mail.ru>
2024-10-03 19:56:36 +0300

    first commit

--- binary_examples.py
+++ binary_examples.py
@@ -1,14 +1,17 @@
 import time
 
+
 def usual_search(list, pow):
     start = time.time()
     item = 2**pow
     index = -1
+    i = 0
     for el in list:
+        i += 1
         index += 1
         if el == item:
             end = time.time()
-            print(f"Usual search: 2**{pow}; index={index} duration {end - start}!")
+            print(f"Usual search: 2**{pow}; iteration={i}; index={index}; duration={end - start};")
             return index
 
 def binary_search(list, pow):
@@ -16,12 +19,14 @@ def binary_search(list, pow):
     item = 2**pow
     low = 0
     high = len(list) - 1
+    i = 0
     while low <= high:
+        i += 1
         mid = (low + high) // 2
         guess = list[mid]
         if guess == item:
             end = time.time()
-            print(f"Binary search: 2**{pow}; index={mid} duration {end - start}!")
+            print(f"Binary search: 2**{pow}; iteration={i}; index={mid}; duration={end - start};")
             return mid
         elif guess > item:
             high = mid - 1
@@ -34,15 +39,11 @@ def binary_search(list, pow):
 
 my_list = range(1, 4_294_967_296)
 
+print("Binary search:")
 for i in range(1, 32):
     binary_search(my_list, i)
+
+print("Usual search:")
+for i in range(1, 32):
     usual_search(my_list, i)
-
-
-
-
-
-
-# print("result_idnex:", binary_search(my_list, 16))
-# print("result_idnex:", binary_search(my_list, -1))
 
