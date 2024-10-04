19526f984835cb6d2bf92355ff7fdcbd93bca58a
artiom1991 <a.stoilos@mail.ru>
2024-10-04 05:24:08 +0300

    first commit

--- recursion.py
+++ recursion.py
@@ -8,5 +8,19 @@ def countdown(i):
     else:                   # Рекурсивный случай
         countdown(i - 1)
 
+# countdown(10)
 
-countdown(10)
+def sum(number):
+    print("Функция запустилась: ", number)           # порядок запуска функций
+    if number <= 1:
+        print("!Функция завершилась: ", number)     # порядок завершения выполнения функций
+        return number
+    else:
+        res = number + sum(number - 1)
+        print("!Функция завершилась: ", number)      # порядок завершения выполнения функций
+        return res
+
+# s = sum(5)
+# s = sum(999)
+s = sum(1000)
+print(s)
